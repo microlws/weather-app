c6bef9d3a1cfbe81bf6caa726c56f1fd8d8e1a4b
microlws <microlws@gmail.com>
2018-05-10 11:57:39 +0200

    added form & weather components

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Titles from 'components/Titles';
+import Form from 'components/Form';
+import Weather from 'components/Weather';
 import './index.scss';
 
 class App extends React.Component {
@@ -7,6 +9,8 @@ class App extends React.Component {
     return (
       <div className='App'>
         <Titles />
+        <Form />
+        <Weather />
       </div>
     );
   }
