1bda97531fe6589c2734a521531d081c627a8879
microlws <microlws@gmail.com>
2018-05-10 13:17:20 +0200

    need help again

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <div>Form Component</div>
+      <form>
+        <input type="text" name="city" placeholder="City" />
+        <input type="text" name="country" placeholder="Country" />
+        <button>Get Weather</button>
+      </form>
     );
   }
 }
--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -4,7 +4,14 @@ import Form from 'components/Form';
 import Weather from 'components/Weather';
 import './index.scss';
 
+const API_KEY = 'e79aecff0a08da73f6f6de08be893f7d';
+
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Aarhus&APPID=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div className='App'>
