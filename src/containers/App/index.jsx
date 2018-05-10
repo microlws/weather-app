f507b949ca2255a559da5fc74297d798ecdfb01d
IgorJoergensen <ij@omnicar.dk>
2018-05-10 14:51:28 +0200

    for my friend

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import Titles from 'components/Titles';
-import Form from 'components/Form';
-import Weather from 'components/Weather';
-import './index.scss';
+import React from 'react'
+import fetch from 'fetch'
+import Titles from 'components/Titles'
+import Form from 'components/Form'
+import Weather from 'components/Weather'
+import './index.scss'
 
-const API_KEY = 'e79aecff0a08da73f6f6de08be893f7d';
+const API_KEY = 'e79aecff0a08da73f6f6de08be893f7d'
+
+const openWeatherMap = async () => {
+  const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Aarhus&APPID=${API_KEY}&units=metric`)
+  const data = apiCall.json()
+  return data
+}
 
 class App extends React.Component {
-  getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Aarhus&APPID=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-    console.log(data);
-  };
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Titles />
-        <Form getWeather={this.getWeather}/>
+        <Form getWeather={openWeatherMap} />
         <Weather />
       </div>
-    );
+    )
   }
-};
-export default App;
+}
+export default App
