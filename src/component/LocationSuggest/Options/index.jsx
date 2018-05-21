a3befdd79a03c37061805a103917acdfdd7e448b
microlws <microlws@gmail.com>
2018-05-21 19:55:05 +0200

    list is now navigatable

--- src/component/LocationSuggest/Options/index.jsx
+++ src/component/LocationSuggest/Options/index.jsx
@@ -4,10 +4,40 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
 import ImageIcon from '@material-ui/icons/Image'
 import './index.scss'
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.actionList = {}
+    this.lastIndex = -1
+  }
+
+  componentDidUpdate() {
+    const { selectedIndex } = this.props
+
+    if (selectedIndex && this.props.isSelecting) {
+      this.actionList[selectedIndex]()
+    } else if (this.actionList[selectedIndex]) {
+      this.blurAll()
+    }
+  }
+
+  componentWillUnmount() {
+    this.blurAll()
+  }
+
+  blurAll = () => {
+    const { actionList } = this
+
+    Object.keys(actionList).forEach(key => {
+      actionList[key](false)
+    })
+  }
+
   handleClick = e => {
     const id = e.currentTarget.id.replace('options-', '')
     let option = []
@@ -22,24 +52,32 @@ class Options extends React.Component {
   }
 
   render() {
-    const { handleClick } = this
+    const { handleClick, actionList } = this
     const { options } = this.props
+
+    console.log('updating list')
 
     return (
       <div className="Options">
         <List className="Options__list">
           {options.map(option => (
-            <ListItem
-              className="Options__item"
+            <Button
+              action={actions => {
+                actionList[option.id] = actions.focusVisible
+              }}
+              className="Options__button"
               id={`options-${option.id}`}
-              key={`options-${option.id}`}
               onClick={handleClick}
+              key={`options-${option.id}`}
+              fullWidth
             >
-              <Avatar>
-                <ImageIcon />
-              </Avatar>
-              <ListItemText primary={option.location} />
-            </ListItem>
+              <ListItem className="Options__item">
+                <Avatar>
+                  <ImageIcon />
+                </Avatar>
+                <ListItemText primary={option.location} />
+              </ListItem>
+            </Button>
           ))}
         </List>
       </div>
@@ -47,7 +85,13 @@ class Options extends React.Component {
   }
 }
 
+Options.defaultProps = {
+  selectedIndex: '',
+}
+
 Options.propTypes = {
+  isSelecting: PropTypes.bool.isRequired,
+  selectedIndex: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(
     PropTypes.shape({
--- src/component/LocationSuggest/index.jsx
+++ src/component/LocationSuggest/index.jsx
@@ -1,16 +1,34 @@
+import InputAdornment from '@material-ui/core/InputAdornment'
 import TextField from '@material-ui/core/TextField'
+import Search from '@material-ui/icons/Search'
 import axios from 'axios'
 import PropTypes from 'prop-types'
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import mouseTrap from 'react-mousetrap'
 import Options from './Options'
 import './index.scss'
+
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
+  },
+})
 
 class LocationSuggest extends React.Component {
   state = {
     options: [],
+    selectedIndex: -1,
+  }
+
+  componentDidMount() {
+    this.props.bindShortcut('up', this.goToPrev)
+    this.props.bindShortcut('down', this.goToNext)
   }
 
   timeout = null
+
+  isSelecting = false
 
   updateOptions = async inputValue => {
     let options = []
@@ -41,31 +59,80 @@ class LocationSuggest extends React.Component {
     const { value } = e.target
 
     clearTimeout(this.timeout)
+    this.isSelecting = false
 
     this.timeout = setTimeout(() => {
       updateOptions(value)
     }, 300)
   }
 
+  goToNext = () => {
+    const { selectedIndex, options } = this.state
+    const checkIndex = selectedIndex + 1
+    const option = options[checkIndex]
+    const newIndex = option ? checkIndex : -1
+
+    if (this.isSelecting) {
+      this.setState({ selectedIndex: newIndex })
+    }
+  }
+
+  goToPrev = () => {
+    const { selectedIndex, options } = this.state
+    const checkIndex = selectedIndex - 1
+    const option = options[checkIndex]
+    const newIndex = option ? checkIndex : -1
+
+    if (this.isSelecting) {
+      this.setState({ selectedIndex: newIndex })
+    }
+  }
+
+  // 40 is down, 38 is up
+  handleKeyDown = e => {
+    if (e.keyCode === 40) {
+      this.goToNext()
+      this.isSelecting = true
+    }
+  }
+
   render() {
+    const { options, selectedIndex } = this.state
+
     return (
       <div className="LocationSuggest">
         <TextField
           fullWidth
           onChange={this.handleInputChange}
-          className="LocationSuggest__input"
+          onKeyDown={this.handleKeyDown}
+          className={this.props.classes.margin}
           label="Location"
-          inputProps={{ placeholder: 'Enter minimum 2 characters to search' }}
+          onFocus={this.handleFocus}
+          InputProps={{
+            placeholder: 'Enter minimum 2 characters to search',
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
+          }}
           defaultValue=""
         />
-        <Options options={this.state.options} onChange={this.props.onChange} />
+        <Options
+          options={options}
+          onChange={this.props.onChange}
+          isSelecting={this.isSelecting}
+          selectedIndex={options[selectedIndex] && options[selectedIndex].id}
+        />
       </div>
     )
   }
 }
 
 LocationSuggest.propTypes = {
+  bindShortcut: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
   onChange: PropTypes.func.isRequired,
 }
 
-export default LocationSuggest
+export default withStyles(styles)(mouseTrap(LocationSuggest))
